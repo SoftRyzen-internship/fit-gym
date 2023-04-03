01977eadd999a8ddb98947043c409ca4ed483c29
GubarievaAnna <annagubarieva@gmail.com>
2023-04-03 16:53:52 +0300

    GM-1: Add slider for section Plans

---
+++ src/assets/js/plansSlider.js
@@ -0,0 +1,31 @@
+$('.plans-slider').slick({
+  lazyLoad: 'ondemand',
+  infinite: true,
+  dots: false,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  mobileFirst: true,
+  speed: 400,
+  cssEase: 'linear',
+  prevArrow: '.plans-prev-btn',
+  nextArrow: '.plans-next-btn',
+  responsive: [
+    {
+      breakpoint: 1279,
+      settings: {
+        slidesToShow: 3,
+        unslick: true,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '26%',
+      },
+    },
+  ],
+});
