4298402b4aaf0446dd5bfbf1fb7aebdabdfaee3e
egarra <garikhabuzov@gmail.com>
2023-04-04 17:52:33 +0300

    FG-15: pop-up in progress add

---
+++ src/assets/js/popUp.js
@@ -0,0 +1,43 @@
+function handleClick({ iconsList, trigger, content }) {
+    const overlay = document.querySelector('.pop-up__overlay');
+    const closeBtn = document.querySelector('.pop-up__icon');
+    const contentBlock = document.querySelector('.pop-up__description');
+    const popUp = document.querySelector('.pop-up');
+    
+  iconsList.addEventListener('click', e => {
+    if (e.target.closest(trigger)) {
+      popUp.classList.remove('pop-up--is-hidden');
+      document.body.style.overflow = 'hidden';
+      contentBlock.innerHTML = content;
+    }
+  });
+
+  overlay.addEventListener('click', e => {
+    if (!e.target.classList.contains('pop-up__overlay')) return;
+    popUp.classList.add('pop-up--is-hidden');
+    document.body.style.overflow = '';
+    contentBlock.innerHTML = '';
+  });
+
+  closeBtn.addEventListener('click', () => {
+    popUp.classList.add('pop-up--is-hidden');
+    document.body.style.overflow = '';
+    contentBlock.innerHTML = '';
+  });
+
+  function closeByEsc(e) {
+    if (e.code === 'Escape') {
+      popUp.classList.add('pop-up--is-hidden');
+      document.body.style.overflow = '';
+      contentBlock.innerHTML = '';
+    }
+  }
+  window.addEventListener('keydown', closeByEsc);
+}
+
+handleClick({
+  iconsList: document.querySelector('.footer__social-list'),
+  trigger: '.footer__social-link',
+  content: `<p class="pop-up__content">Sorry</p>
+    <p class="pop-up__content">This page is in progress</p>`,
+});
